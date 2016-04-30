635e01da6f474ade637f04ddedc26b96a32c8252
danielmawhirter <daniel@mawhirter.com>
2016-04-30 17:31:57 -0600

    sane formatting

--- query.sql
+++ query.sql
@@ -7,9 +7,19 @@ drop table tb_dist;*/
 
 /*separated version of following query*/
 /*create table d_to_sta as (select broadcast_date date, (case when (broadcast_date < '2006-01-01') then 'GHCND:USW00093134' else 'GHCND:USW00054767' end ) station from dond_contestants);
-
 select distinct id, broadcast_date, d_to_sta.station, weather.tmax, weather.tmin, weather.prcp from dond_contestants, weather, d_to_sta where dond_contestants.broadcast_date=d_to_sta.date and weather.station=d_to_sta.station and dond_contestants.broadcast_date=weather.date order by id;*/
 
-select distinct id, broadcast_date, d_to_sta.station, weather.tmax, weather.tmin, weather.prcp from dond_contestants, weather, (select broadcast_date date, (case when (broadcast_date < '2006-01-01') then 'GHCND:USW00093134' else 'GHCND:USW00054767' end ) station from dond_contestants) as d_to_sta where dond_contestants.broadcast_date=d_to_sta.date and weather.station=d_to_sta.station and dond_contestants.broadcast_date=weather.date order by id;
-
+select distinct id, broadcast_date, d_to_sta.station, weather.tmax, weather.tmin, weather.prcp 
+from dond_contestants, weather, (
+  select broadcast_date date, (
+    case when (broadcast_date < '2006-01-01') 
+      then 'GHCND:USW00093134' 
+      else 'GHCND:USW00054767' 
+    end 
+  ) station from dond_contestants
+) as d_to_sta
+where dond_contestants.broadcast_date=d_to_sta.date 
+  and weather.station=d_to_sta.station 
+  and dond_contestants.broadcast_date=weather.date 
+order by id;
 
